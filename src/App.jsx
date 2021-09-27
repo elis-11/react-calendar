f1795d8e014b4e73c79460ce9dba8aeb6832c212
elis-11 <eliza.arzanukaeva@gmail.com>
2021-09-27 20:04:19 +0200

    modified doc.

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import Calendar from './components/Calendar';
+
+class App extends React.Component {
+	state = {
+		date: null
+	};
+
+	handleDateChange = date => this.setState({ date });
+
+	render() {
+		const { date } = this.state;
+
+		return (
+			<div>
+
+				{date && <p>Selected date: {date.toLocaleDateString()}</p>}
+
+				<Calendar
+					onChange={this.handleDateChange}
+				 /> 
+        
+			</div>
+		);
+	}
+}
+export default App;
